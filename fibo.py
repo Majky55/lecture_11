b9183706e571bcb930e98442fe35c3830643858b
Michal Bodi <240485@vut.cz>
2022-05-02 09:46:33 +0200

    my life's work

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
+def recursive_th_fibo(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    else:
+        return recursive_th_fibo(n - 1) + recursive_th_fibo(n - 2)
+
 
 def main():
-    pass
+    print(recursive_th_fibo(27))
 
 
 if __name__ == "__main__":
